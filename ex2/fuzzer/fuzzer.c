71590afadd0d370cf1795d7a1cb44a042ccc145a
Gioele Santi <gioele.santi2@studio.unibo.it>
2025-12-11 11:15:42 +0100

    added ex2

---
+++ ex2/fuzzer/fuzzer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(void) {
+    char token[17] = {0};
+    // TODO: build a fuzzer that finds GhostRead
+    
+    // TODO: read the 16-byte token located at physical address 0x8126cd24 and print it to stdout
+    puts(token);
+}
